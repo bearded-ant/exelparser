2abb47cd09d6dd5e8cb443a2bbc6a4f55af63fcd
ant <theant@mail.ru>
2022-09-28 00:24:18 +0400

    Initial commit

---
+++ src/main/kotlin/com/akopyan/exelparser/ExelparserApplication.kt
@@ -0,0 +1,11 @@
+package com.akopyan.exelparser
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ExelparserApplication
+
+fun main(args: Array<String>) {
+	runApplication<ExelparserApplication>(*args)
+}
---
+++ src/test/kotlin/com/akopyan/exelparser/ExelparserApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.akopyan.exelparser
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ExelparserApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
