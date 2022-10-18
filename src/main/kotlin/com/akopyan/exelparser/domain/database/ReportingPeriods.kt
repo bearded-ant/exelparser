634d6c75e95136b117354d7c9e7d2cd9c9058766
ant <theant@mail.ru>
2022-10-18 23:44:46 +0400

    добавил таблицу reporting periods

---
+++ src/main/kotlin/com/akopyan/exelparser/domain/database/ReportingPeriods.kt
@@ -0,0 +1,14 @@
+package com.akopyan.exelparser.domain.database
+
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+
+@Entity
+data class ReportingPeriods(
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    val id: Int = 0,
+    val reportingPeriod: String =""
+)
