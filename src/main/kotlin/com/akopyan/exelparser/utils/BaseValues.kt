32bb1bbec41a7c7f9a7ba6ae7ba927bc5eaa7106
ant <theant@mail.ru>
2022-10-25 00:47:11 +0400

    добавил константы индексов листов

--- src/main/kotlin/com/akopyan/exelparser/utils/BaseValues.kt
+++ src/main/kotlin/com/akopyan/exelparser/utils/BaseValues.kt
@@ -7,11 +7,13 @@ class BaseValues {
 
     //company
     val SHEET_NAME_C: String = "Detailed franchisee"
+    val SHEET_NAME_C_INDEX: Int = 1
     val COLUM_INDEX_C: ArrayList<Int> = arrayListOf(0, 1, 2, 3, 4, 8, 12, 16, 20, 28, 32, 33)
     val ROW_START_INDEX_C: Int = 7
 
     //employee
     val SHEET_NAME_E: String = "Лист1"
+    val SHEET_NAME_E_INDEX: Int = 0
     val COLUM_INDEX_E: ArrayList<Int> = arrayListOf(0, 2)
     val ROW_START_INDEX_E: Int = 0
 
@@ -23,12 +25,13 @@ class BaseValues {
     val FILE_VALID = "valid name"
 
     //report
-    val NUMERIC_CELL_MAIN: List<Int> = arrayListOf(0,2)
-    val NUMERIC_CELL_DUPLICATES: List<Int> = arrayListOf(1,4)
-    val EMPLOYEE_PATH: String = "/home/ant/employeeReport.xlsx"
+    val NUMERIC_CELL_MAIN: List<Int> = arrayListOf(0, 2)
+    val NUMERIC_CELL_DUPLICATES: List<Int> = arrayListOf(1, 4)
 
-    //    val EMPLOYEE_PATH:String = "employeeReport.xlsx"
-    val DUPLICATES_PATH: String = "/home/ant/duplicateReport.xlsx"
-//    val DUPLICATES_PATH:String = "duplicateReport.xlsx"
+        val EMPLOYEE_PATH: String = "/home/ant/employeeReport.xlsx"
+//    val EMPLOYEE_PATH: String = "employeeReport.xlsx"
+
+        val DUPLICATES_PATH: String = "/home/ant/duplicateReport.xlsx"
+//    val DUPLICATES_PATH: String = "duplicateReport.xlsx"
 
 }
